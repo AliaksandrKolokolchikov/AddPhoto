71b31aae37e15de20a0f8c114ec1069444cebe6d
Aliaksandr Kolokolchikov <aliaksandr kolokolchikov>
2022-04-07 23:50:01 +0300

    new fix footer & header

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
-const openPopUp = document.querySelector('#open_pop_up')
-const closePopUp = document.querySelector('#pop_up_close')
-const popUp = document.querySelector('#pop_up')
+let openPopUp = document.querySelector('#open_pop_up')
+let closePopUp = document.querySelector('#pop_up_close')
+let popUp = document.querySelector('#pop_up')
 
 openPopUp.addEventListener('click', function (e) {
     e.preventDefault();
